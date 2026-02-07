5dabc90e60fba8912b23adf64258218d67070e34
Dobbymin <gmin990624@gmail.com>
2026-02-08 00:29:05 +0900

    chore: emotion 라이브러리 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "node:path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
+      jsxImportSource: "@emotion/react",
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
       },
