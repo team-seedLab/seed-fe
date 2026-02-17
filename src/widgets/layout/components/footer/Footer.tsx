e87e77fff7520b3fd52b3252259903b6f03207d7
Dobbymin <gmin990624@gmail.com>
2026-02-18 00:24:54 +0900

    fix: footer 위치 수정 및 반응형 적용

--- src/widgets/layout/components/footer/Footer.tsx
+++ src/widgets/layout/components/footer/Footer.tsx
@@ -7,45 +7,47 @@ import { ROUTE_PATHS } from "@/shared";
 
 export const Footer = () => {
   return (
-    <Box as="footer" bg="gray.100" h="120px">
-      <Flex
-        h="full"
-        w="full"
-        alignItems="center"
-        justifyContent="space-between"
-        px={4}
-        position="relative"
-      >
-        <Link to={ROUTE_PATHS.ROOT}>
-          <HStack>
-            <Box bg="neutral.900" p={2} rounded="lg">
-              <Image
-                src={Logo}
-                alt="Seed Logo"
-                boxSize={4}
-                objectFit="contain"
-              />
-            </Box>
-            <Text fontWeight="extrabold" fontSize="lg">
-              SEED
+    <Box as="footer" bg="gray.100" w="100vw">
+      <Box maxW={{ base: "100vw", lg: "1280px" }} mx="auto" h="120px">
+        <Flex
+          h="full"
+          w="full"
+          alignItems="center"
+          justifyContent="space-between"
+          px={4}
+          position="relative"
+        >
+          <Link to={ROUTE_PATHS.ROOT}>
+            <HStack>
+              <Box bg="neutral.900" p={2} rounded="lg">
+                <Image
+                  src={Logo}
+                  alt="Seed Logo"
+                  boxSize={4}
+                  objectFit="contain"
+                />
+              </Box>
+              <Text fontWeight="extrabold" fontSize="lg">
+                SEED
+              </Text>
+            </HStack>
+          </Link>
+          <HStack
+            gap={10}
+            position="absolute"
+            left="50%"
+            transform="translateX(-50%)"
+          >
+            <Text color="neutral.600" fontSize="sm">
+              &copy; 2026 Seed Lab. All rights reserved.
             </Text>
           </HStack>
-        </Link>
-        <HStack
-          gap={10}
-          position="absolute"
-          left="50%"
-          transform="translateX(-50%)"
-        >
-          <Text color="neutral.600" fontSize="sm">
-            &copy; 2026 Seed Lab. All rights reserved.
-          </Text>
-        </HStack>
-        <HStack gap={4} fontSize="sm">
-          <Text color="neutral.600">이용 약관</Text>
-          <Text color="neutral.600">개인정보처리방침</Text>
-        </HStack>
-      </Flex>
+          <HStack gap={4} fontSize="sm">
+            <Text color="neutral.600">이용 약관</Text>
+            <Text color="neutral.600">개인정보처리방침</Text>
+          </HStack>
+        </Flex>
+      </Box>
     </Box>
   );
 };
--- src/widgets/layout/ui/RootLayout.tsx
+++ src/widgets/layout/ui/RootLayout.tsx
@@ -6,21 +6,20 @@ import { Footer, Header } from "../components";
 
 export const RootLayout = () => {
   return (
-    <Flex
-      direction="column"
-      maxW={{ base: "100vw", lg: "1280px" }}
-      maxH={{ base: "100dvh", md: "720px" }}
-      w="100vw"
-      h="100dvh"
-      mx="auto"
-    >
-      <Header />
-      <Box mx="auto" w="full" as="main">
-        <Box px={3} flex={1} w="full">
+    <Flex direction="column" minH="100dvh">
+      <Flex
+        direction="column"
+        maxW={{ base: "100vw", lg: "1280px" }}
+        w="100vw"
+        mx="auto"
+        flex={1}
+      >
+        <Header />
+        <Box as="main" flex={1} w="full" mx="auto" px={3} overflowY="auto">
           <Outlet />
         </Box>
-        <Footer />
-      </Box>
+      </Flex>
+      <Footer />
     </Flex>
   );
 };
