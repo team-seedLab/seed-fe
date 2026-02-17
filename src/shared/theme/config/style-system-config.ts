c7e3e959aa6ad0b7bed1ad8929e3533648f0c555
Dobbymin <gmin990624@gmail.com>
2026-02-17 23:11:45 +0900

    feat: chakra ui global style에 color token 값 추가

--- src/shared/theme/config/style-system-config.ts
+++ src/shared/theme/config/style-system-config.ts
@@ -1,12 +1,22 @@
 import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
 
+import { colorSemanticToken, colorToken } from "../../styles";
 import { globalStyles } from "../global";
 
 const config = defineConfig({
   cssVarsRoot: ":where(:root, :host)",
-  cssVarsPrefix: "ck",
+  cssVarsPrefix: "sd",
 
   globalCss: globalStyles,
+
+  theme: {
+    tokens: {
+      colors: colorToken,
+    },
+    semanticTokens: {
+      colors: colorSemanticToken,
+    },
+  },
 });
 
 export const system = createSystem(defaultConfig, config);
