0063118e7a8569808e1b66afc5fa949c67d9d4b7
Dobbymin <gmin990624@gmail.com>
2026-03-07 13:10:27 +0900

    chore: 아이콘작업 완료된 파일 추가

---
+++ src/shared/_assets/icons/index.ts
@@ -0,0 +1,79 @@
+/**
+ * 해당 파일은 자동으로 생성됩니다. 수동으로 수정하지 마세요.
+ * /assets/icons에서 SVG 파일을 추가/삭제한 후 'pnpm generate:icons'를 실행하세요.
+ */
+import _AcademicCapIcon from "@/shared/_assets/icons/academic-cap.svg?react";
+import _AdjustmentsVerticalIcon from "@/shared/_assets/icons/adjustments-vertical.svg?react";
+import _ArrowLeftIcon from "@/shared/_assets/icons/arrow-left.svg?react";
+import _ArrowRightIcon from "@/shared/_assets/icons/arrow-right.svg?react";
+import _BeakerIcon from "@/shared/_assets/icons/beaker.svg?react";
+import _BoardTeacherIcon from "@/shared/_assets/icons/board-teacher.svg?react";
+import _CheckCheckIcon from "@/shared/_assets/icons/check-check.svg?react";
+import _CheckCircleIcon from "@/shared/_assets/icons/check-circle.svg?react";
+import _ChevronRightIcon from "@/shared/_assets/icons/chevron-right.svg?react";
+import _ClipboardCheckIcon from "@/shared/_assets/icons/clipboard-check.svg?react";
+import _CopyIcon from "@/shared/_assets/icons/copy.svg?react";
+import _DocumentTextIcon from "@/shared/_assets/icons/document-text.svg?react";
+import _ErrorIcon from "@/shared/_assets/icons/error.svg?react";
+import _FilePdfIcon from "@/shared/_assets/icons/file-pdf.svg?react";
+import _FilePenIcon from "@/shared/_assets/icons/file-pen.svg?react";
+import _MagnifyingGlassIcon from "@/shared/_assets/icons/magnifying-glass.svg?react";
+import _MicroscopeIcon from "@/shared/_assets/icons/microscope.svg?react";
+import _OrgChartIcon from "@/shared/_assets/icons/org-chart.svg?react";
+import _PictureIcon from "@/shared/_assets/icons/picture.svg?react";
+import _PlusCircleIcon from "@/shared/_assets/icons/plus-circle.svg?react";
+import _PlusIcon from "@/shared/_assets/icons/plus.svg?react";
+import _ScriptIcon from "@/shared/_assets/icons/script.svg?react";
+import _SettingIcon from "@/shared/_assets/icons/setting.svg?react";
+import _SparklesIcon from "@/shared/_assets/icons/sparkles.svg?react";
+import _SquarePlayIcon from "@/shared/_assets/icons/square-play.svg?react";
+import _TerminalIcon from "@/shared/_assets/icons/terminal.svg?react";
+import _XMarkIcon from "@/shared/_assets/icons/x-mark.svg?react";
+
+import { createIcon } from "../../components";
+
+export const AcademicCapIcon = createIcon(_AcademicCapIcon, "AcademicCapIcon");
+export const AdjustmentsVerticalIcon = createIcon(
+  _AdjustmentsVerticalIcon,
+  "AdjustmentsVerticalIcon",
+);
+export const ArrowLeftIcon = createIcon(_ArrowLeftIcon, "ArrowLeftIcon");
+export const ArrowRightIcon = createIcon(_ArrowRightIcon, "ArrowRightIcon");
+export const BeakerIcon = createIcon(_BeakerIcon, "BeakerIcon");
+export const BoardTeacherIcon = createIcon(
+  _BoardTeacherIcon,
+  "BoardTeacherIcon",
+);
+export const CheckCheckIcon = createIcon(_CheckCheckIcon, "CheckCheckIcon");
+export const CheckCircleIcon = createIcon(_CheckCircleIcon, "CheckCircleIcon");
+export const ChevronRightIcon = createIcon(
+  _ChevronRightIcon,
+  "ChevronRightIcon",
+);
+export const ClipboardCheckIcon = createIcon(
+  _ClipboardCheckIcon,
+  "ClipboardCheckIcon",
+);
+export const CopyIcon = createIcon(_CopyIcon, "CopyIcon");
+export const DocumentTextIcon = createIcon(
+  _DocumentTextIcon,
+  "DocumentTextIcon",
+);
+export const ErrorIcon = createIcon(_ErrorIcon, "ErrorIcon");
+export const FilePdfIcon = createIcon(_FilePdfIcon, "FilePdfIcon");
+export const FilePenIcon = createIcon(_FilePenIcon, "FilePenIcon");
+export const MagnifyingGlassIcon = createIcon(
+  _MagnifyingGlassIcon,
+  "MagnifyingGlassIcon",
+);
+export const MicroscopeIcon = createIcon(_MicroscopeIcon, "MicroscopeIcon");
+export const OrgChartIcon = createIcon(_OrgChartIcon, "OrgChartIcon");
+export const PictureIcon = createIcon(_PictureIcon, "PictureIcon");
+export const PlusCircleIcon = createIcon(_PlusCircleIcon, "PlusCircleIcon");
+export const PlusIcon = createIcon(_PlusIcon, "PlusIcon");
+export const ScriptIcon = createIcon(_ScriptIcon, "ScriptIcon");
+export const SettingIcon = createIcon(_SettingIcon, "SettingIcon");
+export const SparklesIcon = createIcon(_SparklesIcon, "SparklesIcon");
+export const SquarePlayIcon = createIcon(_SquarePlayIcon, "SquarePlayIcon");
+export const TerminalIcon = createIcon(_TerminalIcon, "TerminalIcon");
+export const XMarkIcon = createIcon(_XMarkIcon, "XMarkIcon");
