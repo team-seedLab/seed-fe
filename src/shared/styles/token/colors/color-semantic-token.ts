36060844f671aec63a32280f2914e1e8a08af73c
TTOCHIwas <95687307+TTOCHIwas@users.noreply.github.com>
2026-03-08 22:54:23 +0900

    feat: 색상 토큰 추가

--- src/shared/styles/token/colors/color-semantic-token.ts
+++ src/shared/styles/token/colors/color-semantic-token.ts
@@ -1,7 +1,20 @@
 export const colorSemanticToken = {
   seed: {
-    DEFAULT: { value: "#98C95C" },
-    hover: { value: "#82b34a" },
-    active: { value: "#5FA23A" },
+    DEFAULT: { value: "{colors.seed.400}" },
+    hover: { value: "{colors.seed.500}" },
+    active: { value: "{colors.seed.600}" },
+    muted: { value: "{colors.seed.200}" },
+    subtle: { value: "{colors.seed.50}" },
+    solid: { value: "{colors.seed.400}" },
+    contrast: { value: "{colors.neutral.0}" },
+  },
+  pdf: {
+    DEFAULT: { value: "{colors.red.500}" },
+    solid: { value: "{colors.red.100}" },
+  },
+  text: {
+    DEFAULT: { value: "{colors.neutral.950}" },
+    secondary: { value: "{colors.neutral.400}" },
+    placeholder: { value: "{colors.neutral.200}" },
   },
 };
