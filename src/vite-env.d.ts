2f78f6a3219a466d1f966250e5cfa10ab25409f1
Dobbymin <gmin990624@gmail.com>
2026-03-15 02:04:05 +0900

    feat(auth): 전역 환경 변수 타입 설정에 서버 주소 관련 타입 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_KAKAO_LOGIN_URL?: string;
   readonly VITE_KAKAO_LOGIN_URL_LOCAL?: string;
   readonly VITE_KAKAO_LOGIN_URL_PROD?: string;
+  readonly VITE_SERVER_URL_LOCAL?: string;
+  readonly VITE_SERVER_URL_PROD?: string;
 }
 
 interface ImportMeta {
