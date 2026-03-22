5b3e3569e6731f0b2c221ccb46558948adde3726
TTOCHIwas <95687307+TTOCHIwas@users.noreply.github.com>
2026-03-22 22:28:01 +0900

    feat: 과제물 유형 이름 한글 매핑 추가

--- src/entities/project/model/constants/index.ts
+++ src/entities/project/model/constants/index.ts
@@ -1,1 +1,2 @@
 export * from "./project-query-keys";
+export * from "./roadmap-type";
---
+++ src/entities/project/model/constants/roadmap-type.ts
@@ -0,0 +1,8 @@
+export const ROADMAP_TYPE_LABEL: Record<string, string> = {
+  REPORT: "글쓰기형",
+  PAPER: "논문형",
+  PRESENTATION: "발표형",
+  EXPERIMENT: "실습형",
+  STUDY_SUMMARY: "요약형",
+  STUDY_LEARNING: "학습형",
+};
