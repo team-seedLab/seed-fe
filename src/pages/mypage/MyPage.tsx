485097b90353eb7fefd36216c32f3fcc66cb4339
TTOCHIwas <95687307+TTOCHIwas@users.noreply.github.com>
2026-04-14 23:04:02 +0900

    feat:마이페이지 상단 반응형 조정

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -5,7 +5,13 @@ import { ProjectListSection, UserNameSection } from "@/features";
 export default function MyPage() {
   return (
     <Flex align="center" bg="container.bg" direction="column" minH="100vh">
-      <Flex direction="column" maxW="1024px" w="full" py={16} px={6}>
+      <Flex
+        direction="column"
+        maxW="1024px"
+        px={{ base: 4, md: 6 }}
+        py={{ base: 10, md: 16 }}
+        w="full"
+      >
         <UserNameSection />
         <ProjectListSection />
       </Flex>
